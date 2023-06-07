b78a8c3a10dc13bb99383033d0f63cb5f4bec242
kartikth40 <kartikth40@gmail.com>
2023-06-07 23:14:41 +0530

    added next.js image component and changed the website's theme to light

--- components/Home/PostCard.tsx
+++ components/Home/PostCard.tsx
@@ -1,15 +1,27 @@
 import { post } from '@/services'
 import styles from '../../app/page.module.scss'
+import moment from 'moment'
+import Link from 'next/link'
+import Image from 'next/image'
 
 type Props = { post: post }
 
 function PostCard({ post }: Props) {
-  const { postCard } = styles
+  const { postCard, postImage, title, summary, authorName } = styles
   return (
     <div className={postCard}>
-      <div>{post.title}</div>
-      <div>{post.summary}</div>
-      <div>{post.author.name}</div>
+      <div className={postImage}>
+        <Image
+          src={post.featuredImage.url}
+          style={{ objectFit: 'cover', borderRadius: '.5rem' }}
+          fill={true}
+          alt={post.title}
+        />
+      </div>
+
+      <div className={title}>{post.title}</div>
+      <div className={summary}>{post.summary}</div>
+      <div className={authorName}>{post.author.name}</div>
     </div>
   )
 }
