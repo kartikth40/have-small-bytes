77164650f16a55812329f17f968d1611d6819c2f
kartikth40 <kartikth40@gmail.com>
2023-06-08 17:51:54 +0530

    made post card component

--- components/Home/PostCard.tsx
+++ components/Home/PostCard.tsx
@@ -7,7 +7,16 @@ import Image from 'next/image'
 type Props = { post: post }
 
 function PostCard({ post }: Props) {
-  const { postCard, postImage, title, summary, authorName } = styles
+  const {
+    postCard,
+    postImage,
+    title,
+    summary,
+    authorName,
+    authorInfo,
+    authorInfoContainer,
+    authorImage,
+  } = styles
   return (
     <div className={postCard}>
       <div className={postImage}>
@@ -19,9 +28,26 @@ function PostCard({ post }: Props) {
         />
       </div>
 
-      <div className={title}>{post.title}</div>
+      <h1>
+        <Link href={`/post/${post.slug}`} className={title}>
+          {post.title}
+        </Link>
+      </h1>
       <div className={summary}>{post.summary}</div>
-      <div className={authorName}>{post.author.name}</div>
+      <div className={authorInfoContainer}>
+        <Link href={`author/${post.author.id}`} className={authorInfo}>
+          <div className={authorImage}>
+            <Image
+              src={post.author.photo.url}
+              style={{ objectFit: 'cover', borderRadius: '50%' }}
+              sizes="(max-width: 768px) 40px, (max-width: 1200px) 50px, 40px"
+              fill={true}
+              alt={post.author.name}
+            />
+          </div>
+          <div className={authorName}>{post.author.name}</div>
+        </Link>
+      </div>
     </div>
   )
 }
