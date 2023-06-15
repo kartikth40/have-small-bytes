dd9ea0676f70ca98a9d2ec6930f8ebc888eb9244
kartikth40 <kartikth40@gmail.com>
2023-06-15 19:20:53 +0530

    added link in footer to my portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,25 @@
+import Link from 'next/link'
 import styles from '../app/page.module.scss'
 
 type Props = {}
 
 export default function Footer({}: Props) {
   const { footerContainer, footer, logo, tagLine, copyrights } = styles
+  const currentDate = new Date().getFullYear()
   return (
     <div className={footerContainer}>
       <div className={footer}>
         <div className={logo}>HSB</div>
-        <div className={tagLine}>Byte Size Knowledge</div>
-        <div className={copyrights}>© Kartik Thakur 2023</div>
+        <div className={tagLine}>Have Small Bytes</div>
+        <div className={copyrights}>
+          <Link
+            href="https://kartik-thakur.tech/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            © Kartik Thakur {currentDate}
+          </Link>
+        </div>
       </div>
     </div>
   )
