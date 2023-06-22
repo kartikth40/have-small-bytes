ba3a3ee32c8a8c75f24e42f2c06c2a93142bf1f2
kartikth40 <kartikth40@gmail.com>
2023-06-22 18:08:11 +0530

    added redirection if not signed in to account

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,7 +3,7 @@ export const revalidate = 60
 import { notFound, redirect } from 'next/navigation'
 import Author from '@/components/BlogPost/Author'
 import BlogPost from '@/components/BlogPost/BlogPost'
-import { getPostDetails, getPosts } from '@/services'
+import { getPostDetails } from '@/services'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
@@ -21,7 +21,7 @@ export default async function Blog({ params }: Props) {
   const session = await getServerSession(authOptions)
 
   if (!session) {
-    redirect(`/signin?callbackUrl=/post/${params.slug}`)
+    redirect(`/api/auth/signin?callbackUrl=/post/${params.slug}`)
   }
   const post = await getPostDetails(params.slug)
   if (!post) return notFound()
--- app/reader/profile/page.tsx
+++ app/reader/profile/page.tsx
@@ -1,7 +1,23 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import SignOutButton from '@/components/buttons/SignOutButton '
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
 
-export default function ProfilePage({}: Props) {
-  return <div>Profile Page.</div>
+export default async function ProfilePage({}: Props) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect(`/api/auth/signin?callbackUrl=/reader/profile`)
+  }
+  return (
+    <div>
+      Profile Page.{' '}
+      <div>
+        <SignOutButton />
+      </div>
+    </div>
+  )
 }
