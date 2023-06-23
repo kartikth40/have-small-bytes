0e2d8341bf0deb2890b819c0e5df1f09bc721ff6
kartikth40 <kartikth40@gmail.com>
2023-06-23 21:22:40 +0530

    added github oAuth login capability

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,22 +1,48 @@
 'use client'
 
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 import styles from './page.module.scss'
-import { getSession, signIn } from 'next-auth/react'
+import {
+  ClientSafeProvider,
+  LiteralUnion,
+  getProviders,
+  signIn,
+  useSession,
+} from 'next-auth/react'
 import { redirect, useSearchParams } from 'next/navigation'
+import { BuiltInProviderType } from 'next-auth/providers'
 
 type Props = {}
 
-export default async function LoginPage({}: Props) {
-  const session = await getSession()
-
-  if (session && session.user) {
-    redirect('/reader/profile')
+export default function LoginPage({}: Props) {
+  const { data: session } = useSession()
+  if (session) {
+    redirect('/')
   }
+
+  const [providers, setProviders] = useState<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>(null)
+
+  useEffect(() => {
+    async function setP() {
+      const res = await getProviders()
+      console.log(res)
+      setProviders(res)
+    }
+    setP()
+  }, [])
+
   const username = useRef('')
   const password = useRef('')
-  const { headingsContainer, mainContainer, register, loginBtnContainer } =
-    styles
+  const {
+    headingsContainer,
+    mainContainer,
+    register,
+    loginBtnContainer,
+    thirdPartyLoginContainer,
+  } = styles
   const callbakUrl = useSearchParams().get('callbackUrl')
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -28,53 +54,76 @@ export default async function LoginPage({}: Props) {
     })
   }
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <div className={headingsContainer}>
-        {/* <h3>Sign in</h3> */}
-        <p>Sign in with your username and password</p>
-      </div>
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className={headingsContainer}>
+          {/* <h3>Sign in</h3> */}
+          <p>Sign in with your username and password</p>
+        </div>
 
-      <div className={mainContainer}>
-        <label htmlFor="username">Your username</label>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="username"
-          onChange={(e) => {
-            username.current = e.target.value
-          }}
-          required
-        />
+        <div className={mainContainer}>
+          <label htmlFor="username">Your username</label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            onChange={(e) => {
+              username.current = e.target.value
+            }}
+            required
+          />
 
-        <br />
-        <br />
+          <br />
+          <br />
 
-        <label htmlFor="pswrd">Your password</label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="pswrd"
-          onChange={(e) => {
-            password.current = e.target.value
-          }}
-          required
-        />
-        <br />
-        <br />
-        <div>
-          <p>
-            <a href="#">Forgot Password?</a>
+          <label htmlFor="pswrd">Your password</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="pswrd"
+            onChange={(e) => {
+              password.current = e.target.value
+            }}
+            required
+          />
+          <br />
+          <br />
+          <div>
+            <p>
+              <a href="#">Forgot Password?</a>
+            </p>
+          </div>
+          <br />
+          <div className={loginBtnContainer}>
+            <button type="submit">Sign in</button>
+          </div>
+
+          <p className={register}>
+            Not registered? <a href="#">Register here!</a>
           </p>
-        </div>
-        <br />
-        <div className={loginBtnContainer}>
-          <button type="submit">Sign in</button>
-        </div>
 
-        <p className={register}>
-          Not registered? <a href="#">Register here!</a>
-        </p>
-      </div>
-    </form>
+          <div className={thirdPartyLoginContainer}>
+            {providers
+              ? Object.values(providers).map((provider) =>
+                  provider.name !== 'Credentials' ? (
+                    <div key={provider.name}>
+                      <button onClick={() => signIn(provider.id)}>
+                        <img
+                          loading="lazy"
+                          id="provider-logo-dark"
+                          src={`https://authjs.dev/img/providers/${provider.id}-dark.svg`}
+                          width="24"
+                          height="24"
+                        />
+                        Sign in with {provider.name}
+                      </button>
+                    </div>
+                  ) : null
+                )
+              : null}
+          </div>
+        </div>
+      </form>
+    </>
   )
 }
