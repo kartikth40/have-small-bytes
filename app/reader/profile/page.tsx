3f211d4336b603dcad98f72eff1692bd41e3a57c
kartikth40 <kartikth40@gmail.com>
2023-06-28 20:37:18 +0530

    fixed issue that was slowing down the browser

--- app/reader/profile/page.tsx
+++ app/reader/profile/page.tsx
@@ -1,51 +1,40 @@
+'use client'
 import SignOutButton from '@/components/buttons/SignOutButton '
 import { redirect } from 'next/navigation'
 import styles from './page.module.scss'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSession } from 'next-auth/react'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-
 type Props = {}
 
-export default async function ProfilePage({}: Props) {
-  const session = await getServerSession(authOptions)
-  // const loading = status === 'loading'
+export default function ProfilePage({}: Props) {
+  const { data: session, status } = useSession()
+  const loading = status === 'loading'
 
-  if (!session) {
+  if (!session && !loading) {
     redirect(`/api/auth/signin?callbackUrl=/reader/profile`)
   }
 
-  // const [selected, setSelected] = useState<string>('profile')
+  const [selected, setSelected] = useState<string>('profile')
 
   const { profilePageContainer, mainContainer, sideMenu, content } = styles
   return (
     <div className={profilePageContainer}>
-      <h2>Profile Page</h2>
+      <h2>Your Profile</h2>
 
       <div className={mainContainer}>
         <div className={sideMenu}>
-          <ul>
-            <li
-            // className={`${
-            //   selected === 'profile' ? 'selected' : 'unselected'
-            // }`}
-            >
-              Profile
-            </li>
-            <li
-            // className={`${
-            //   selected === 'signout' ? 'selected' : 'unselected'
-            // }`}
-            >
-              Sign Out
-            </li>
-          </ul>
+          <div
+            onClick={() => {
+              setSelected('profile')
+            }}
+          >
+            Profile
+          </div>
+          <div>
+            <SignOutButton />
+          </div>
         </div>
-        <div className={content}>
-          {/* {selected === 'profile' && 'Profile Pic'}
-          {selected === 'signout' && <SignOutButton />} */}
-        </div>
+        <div className={content}>{selected === 'profile' && 'Profile Pic'}</div>
       </div>
     </div>
   )
