71169d337c2436aaa122e4eb1a8b206e6db1815b
kartikth40 <kartikth40@gmail.com>
2023-07-04 23:58:51 +0530

    added feedback icons on post details page

--- components/AsidePost.tsx
+++ components/AsidePost.tsx
@@ -2,9 +2,11 @@ import styles from '../app/page.module.scss'
 import { getPostDetails } from '@/services'
 import SimilarWidget from './SimilarWidget'
 import { notFound } from 'next/navigation'
+import LikeButton from './buttons/LikeButton'
+import CommentButton from './buttons/CommentButton'
 
 async function Aside({ slug }: { slug: string }) {
-  const { aside } = styles
+  const { aside, like, comment, userFeedbackContainerAside } = styles
   const post = await getPostDetails(slug)
   if (!post) return notFound()
 
@@ -14,6 +16,16 @@ async function Aside({ slug }: { slug: string }) {
         slug={post!.slug}
         categories={post!.categories.map((category) => category.slug)}
       />
+      <div className={userFeedbackContainerAside}>
+        <div className={like}>
+          <LikeButton />
+          <span>0</span>
+        </div>
+        <div className={comment}>
+          <CommentButton />
+          <span>0</span>
+        </div>
+      </div>
     </aside>
   )
 }
