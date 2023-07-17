e953b94b7515a1975bbdd35290a188f35236846e
kartikth40 <kartikth40@gmail.com>
2023-07-17 19:39:47 +0530

    fixed errors on production build

--- components/BlogPost/ReactSyntaxHighlighter.tsx
+++ components/BlogPost/ReactSyntaxHighlighter.tsx
@@ -1,18 +1,19 @@
 'use client'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 type Props = { children: string; language: string }
 
 export default function ReactSyntaxHighlighter({ children, language }: Props) {
   return (
     <SyntaxHighlighter
-      children={children}
       language={language}
-      style={atomDark}
+      style={dracula}
       wrapLongLines
       wrapLines
       customStyle={{ background: '#171717' }}
-    ></SyntaxHighlighter>
+    >
+      {children}
+    </SyntaxHighlighter>
   )
 }
