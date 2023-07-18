8d5cf0937d26bbb005ff7d9508d37a32dc11257a
kartikth40 <kartikth40@gmail.com>
2023-07-18 21:49:21 +0530

    added themed css variables

---
+++ components/global/ThemeContext.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { useState } from 'react'
+type Theme = 'light' | 'dark'
+type ThemeContext = { theme: Theme; toggleTheme: () => void }
+
+export const ThemeContext = React.createContext<ThemeContext>(
+  {} as ThemeContext
+)
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>('light')
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  const color = theme === 'light' ? '#333' : '#FFF'
+  const backgroundColor = theme === 'light' ? '#FFF' : '#333'
+
+  document.body.style.color = color
+  document.body.style.backgroundColor = backgroundColor
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
