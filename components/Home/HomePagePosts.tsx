582ea7003356c255c19afffc2f795cf732d8c735
kartikth40 <kartikth40@gmail.com>
2023-07-25 22:38:55 +0530

    changed pagination to load more button which will load 3 posts per click

--- components/Home/HomePagePosts.tsx
+++ components/Home/HomePagePosts.tsx
@@ -1,32 +1,18 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { getCategoryPosts, getPosts } from '@/services'
 import styles from '../../app/page.module.scss'
 import PostCard from './PostCard'
 import { postsType } from '@/utils/types/types'
-import PaginationButton from '../buttons/PaginationButton'
+import LoadMoreBtn from '../buttons/LoadMoreBtn'
+import { getCategoryPosts, getPosts } from '@/services'
 
 type Props = { categorySlug?: string }
 
-function HomePagePosts({ categorySlug = '' }: Props) {
-  const postsPerPage = 3
-  const [posts, setPosts] = useState<postsType[] | []>([])
-  const [pageNo, setPageNo] = useState<number>(1)
-
-  useEffect(() => {
-    async function setAllPosts() {
-      // window.scrollTo(0, 0)
-      if (categorySlug) {
-        setPosts(
-          await getCategoryPosts(categorySlug, (pageNo - 1) * postsPerPage)
-        )
-      } else {
-        setPosts(await getPosts((pageNo - 1) * postsPerPage))
-      }
-    }
-    setAllPosts()
-  }, [pageNo])
+async function HomePagePosts({ categorySlug = '' }: Props) {
+  let posts: postsType[]
+  if (!categorySlug) {
+    posts = await getPosts()
+  } else {
+    posts = await getCategoryPosts(categorySlug)
+  }
 
   const { postsContainer } = styles
 
@@ -35,11 +21,7 @@ function HomePagePosts({ categorySlug = '' }: Props) {
       {posts?.map((post) => (
         <PostCard post={post} key={post.title}></PostCard>
       ))}
-      <PaginationButton
-        pageNo={pageNo}
-        setPageNo={setPageNo}
-        categorySlug={categorySlug}
-      />
+      <LoadMoreBtn categorySlug={categorySlug} />
     </section>
   )
 }
---
+++ components/buttons/LoadMoreBtn.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import styles from '../../app/page.module.scss'
+import { getCategoryPosts, getPosts } from '@/services'
+import { postsType } from '@/utils/types/types'
+import PostCard from '../Home/PostCard'
+
+type Props = {
+  categorySlug: string
+}
+
+export default function LoadMoreBtn({ categorySlug }: Props) {
+  const postsPerLoad = 3
+  const [morePosts, setMorePosts] = useState<postsType[] | []>([])
+  const [loadMore, setLoadMore] = useState<boolean>(true)
+  const [loadNumber, setLoadNumber] = useState<number>(0)
+
+  useEffect(() => {
+    async function loading() {
+      let newLoadedPosts: [postsType] | []
+      if (!categorySlug) {
+        newLoadedPosts = await getPosts(loadNumber * postsPerLoad)
+      } else {
+        newLoadedPosts = await getCategoryPosts(
+          categorySlug,
+          loadNumber * postsPerLoad
+        )
+      }
+      if (newLoadedPosts.length < postsPerLoad) {
+        setLoadMore(false)
+      } else {
+        setLoadMore(true)
+      }
+      setMorePosts((prev) => [...prev, ...newLoadedPosts])
+    }
+    if (loadNumber === 0 || !loadMore) return
+    loading()
+  }, [loadNumber])
+
+  const handleLoadMore = () => {
+    if (loadMore) setLoadNumber((prev) => prev + 1)
+  }
+
+  const { loadMoreContainer, noMore } = styles
+
+  return (
+    <>
+      {morePosts?.map((post) => (
+        <PostCard post={post} key={post.title}></PostCard>
+      ))}
+      <div
+        className={`${loadMoreContainer} ${!loadMore && noMore}`}
+        onClick={handleLoadMore}
+      >
+        {loadMore ? 'Load More...' : 'Thats all for now :)'}
+      </div>
+    </>
+  )
+}
