a8d6002eec2955067169b13bd156c4284867c652
kartikth40 <kartikth40@gmail.com>
2023-08-02 11:47:52 +0530

    added header component mobile structure to load on mobile screens

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -17,6 +17,9 @@ function Header({}: Props) {
   const [categories, setCategories] = useState<
     categoriesType['categories'] | []
   >([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [mobile, setMobile] = useState<boolean>(false)
+
   const windowSize = useWindowSize()
   useEffect(() => {
     async function setCat() {
@@ -25,36 +28,69 @@ function Header({}: Props) {
     setCat()
   }, [])
 
-  const { header, nav, logo, navLink } = styles
+  useEffect(() => {
+    if (categories.length === 0) {
+      setLoading(true)
+    } else {
+      setLoading(false)
+    }
+  }, [categories])
+
+  useEffect(() => {
+    if (windowSize && windowSize <= 500) {
+      setMobile(true)
+    } else {
+      setMobile(false)
+    }
+  }, [windowSize])
+
+  const { header, nav, logo, navLink, loadingBtn, firstHeaderRow, hideMe } =
+    styles
   console.log(windowSize, screenSize.mobile)
 
   return (
     <header className={header}>
-      <div className={logo}>
-        <Link href="/">
-          <Image
-            src="/icons/hsb-icon.png"
-            style={{ objectFit: 'cover' }}
-            // sizes="(max-width: 768px) 40px, (max-width: 1200px) 50px, 40px"
-            width={40}
-            height={40}
-            alt={'Logo'}
-          />
-          <span>Have Small Bytes</span>
-        </Link>
-      </div>
-      <nav className={nav}>
-        {categories.map((category) => (
-          <Link
-            key={category.slug}
-            href={`category/${category.slug}`}
-            className={navLink}
-          >
-            {category.name}
+      <div className={firstHeaderRow}>
+        <div className={logo}>
+          <Link href="/">
+            <Image
+              src="/icons/hsb-icon.png"
+              style={{ objectFit: 'cover' }}
+              width={40}
+              height={40}
+              alt={'Logo'}
+            />
+            <span>Have Small Bytes</span>
           </Link>
-        ))}
-        <ThemeToggleButton />
-        <SignInButton />
+        </div>
+        <nav className={`${nav} ${!mobile && hideMe}`}>
+          <ThemeToggleButton />
+          <SignInButton />
+        </nav>
+      </div>
+
+      <nav className={nav}>
+        {loading
+          ? ['Web Development', 'DSA', 'Personal Development'].map(
+              (cat, idx) => (
+                <span key={idx} className={`${loadingBtn} ${navLink}`}>
+                  {cat}
+                </span>
+              )
+            )
+          : categories.map((category) => (
+              <Link
+                key={category.slug}
+                href={`category/${category.slug}`}
+                className={navLink}
+              >
+                {category.name}
+              </Link>
+            ))}
+        <span className={`${nav} ${mobile && hideMe}`}>
+          <ThemeToggleButton />
+          <SignInButton />
+        </span>
       </nav>
     </header>
   )
