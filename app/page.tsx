ac16f025f60dd7f4629fec069cb1be1571987f1b
kartikth40 <kartikth40@gmail.com>
2023-08-06 20:53:38 +0530

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 export const revalidate = 60
 
 import FeaturedPosts from '@/components/Home/FeaturedPosts'
+import HeroSection from '@/components/Home/HeroSection'
 import PageContent from '@/components/Home/PageContent'
 
 export default function Home() {
   return (
     <main>
+      <HeroSection />
       <FeaturedPosts />
       <PageContent />
     </main>
