7b7901b5b8cdecfa8be17ce4a40225c56507f5fe
kartikth40 <kartikth40@gmail.com>
2023-08-27 15:26:32 +0530

    trying to fix nodemailer production issue

--- services/emailService.js
+++ services/emailService.js
@@ -1,34 +1,21 @@
 var nodemailer = require('nodemailer')
 //-----------------------------------------------------------------------------
 export async function sendMail(subject, toEmail, htmlBody) {
-  var transporter = nodemailer.createTransport({
-    port: 465,
-    host: 'smtp.gmail.com',
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PW,
     },
-    secure: true,
   })
 
-  var mailOptions = {
+  const mailOptions = {
     from: process.env.NODEMAILER_EMAIL,
     to: toEmail,
     subject: subject,
     html: htmlBody,
   }
 
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailOptions, (err, response) => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        console.log(response)
-        resolve(response)
-      }
-    })
-  })
+  // send mail
+  await transporter.sendMail(mailOptions)
 }
