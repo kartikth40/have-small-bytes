29c752c31c9b1840673fc52e886b99bab93fd856
Kartik Thakur <kartikth40@gmail.com>
2023-09-11 23:47:54 +0530

    trying to fix dynamic metadata generation

--- app/category/[cat]/page.tsx
+++ app/category/[cat]/page.tsx
@@ -5,7 +5,31 @@ import HeroText from '@/components/categories/HeroText'
 import { getCategories } from '@/services'
 import { notFound } from 'next/navigation'
 
-export default async function Blog({ params }: { params: { cat: string } }) {
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { cat: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // fetch data
+  const categories = await getCategories()
+  const category = categories.find((cat) => cat.slug === params.cat)
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: category?.name ?? 'HSB',
+    // description:
+    //   'Category based blogs about web development, data structures and algorithms and personal development',
+    // openGraph: {
+    //   images: ['/some-specific-page-image.jpg', ...previousImages],
+    // },
+  }
+}
+
+export default async function Blog({ params }: Props) {
   const categories = await getCategories()
 
   const category = categories.find((cat) => cat.slug === params.cat)
--- app/post/[slug]/layout.tsx
+++ app/post/[slug]/layout.tsx
@@ -1,32 +1,5 @@
 import styles from './page.module.scss'
 import Aside from '@/components/AsidePost'
-import { getPostBySlug, getPosts } from '@/services'
-
-import type { Metadata, ResolvingMetadata } from 'next'
-
-type Props = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // fetch data
-  const post = await getPostBySlug(params.slug)
-
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || []
-
-  return {
-    title: post?.title ?? 'HSB',
-    description: post?.summary,
-    openGraph: {
-      images: ['/some-specific-page-image.jpg', ...previousImages],
-    },
-  }
-}
 
 export default function Layout({
   children,
