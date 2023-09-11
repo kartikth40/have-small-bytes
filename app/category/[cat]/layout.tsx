4281eea50950e9082ba42c3b8513ba7a64958de5
Kartik Thakur <kartikth40@gmail.com>
2023-09-11 23:21:57 +0530

    fixing metadata generation - try no. 2

--- app/category/[cat]/layout.tsx
+++ app/category/[cat]/layout.tsx
@@ -5,23 +5,27 @@ import type { Metadata, ResolvingMetadata } from 'next'
 
 type Props = {
   params: { cat: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   // fetch data
   const categories = await getCategories()
   const category = categories.find((cat) => cat.slug === params.cat)
 
   // optionally access and extend (rather than replace) parent metadata
-  // const previousImages = (await parent).openGraph?.images || []
+  const previousImages = (await parent).openGraph?.images || []
 
   return {
     title: category?.name ?? 'HSB',
     description:
       'Category based blogs about web development, data structures and algorithms and personal development',
-    // openGraph: {
-    //   images: ['/some-specific-page-image.jpg', ...previousImages],
-    // },
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
   }
 }
 
