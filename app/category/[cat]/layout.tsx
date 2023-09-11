a5f820503bb18c7934127c9b9695b971042f723f
Kartik Thakur <kartikth40@gmail.com>
2023-09-11 23:30:43 +0530

    trying to fix dynamic metadata generation

--- app/category/[cat]/layout.tsx
+++ app/category/[cat]/layout.tsx
@@ -1,29 +1,29 @@
-import { getCategories } from '@/services'
+// import { getCategories } from '@/services'
 import styles from './page.module.scss'
 
-import type { Metadata, ResolvingMetadata } from 'next'
+// import type { Metadata, ResolvingMetadata } from 'next'
 
-type Props = {
-  params: { cat: string }
-}
+// type Props = {
+//   params: { cat: string }
+// }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // fetch data
-  const categories = await getCategories()
-  const category = categories.find((cat) => cat.slug === params.cat)
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   // fetch data
+//   const categories = await getCategories()
+//   const category = categories.find((cat) => cat.slug === params.cat)
 
-  // optionally access and extend (rather than replace) parent metadata
-  // const previousImages = (await parent).openGraph?.images || []
+//   // optionally access and extend (rather than replace) parent metadata
+//   // const previousImages = (await parent).openGraph?.images || []
 
-  return {
-    title: category?.name ?? 'HSB',
-    // description:
-    //   'Category based blogs about web development, data structures and algorithms and personal development',
-    // openGraph: {
-    //   images: ['/some-specific-page-image.jpg', ...previousImages],
-    // },
-  }
-}
+//   return {
+//     title: category?.name ?? 'HSB',
+//     // description:
+//     //   'Category based blogs about web development, data structures and algorithms and personal development',
+//     // openGraph: {
+//     //   images: ['/some-specific-page-image.jpg', ...previousImages],
+//     // },
+//   }
+// }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { pageLayout } = styles
