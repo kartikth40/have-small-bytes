19750993ba21f7e571bc5dc32cd6c0be7c84effa
Kartik Thakur <kartikth40@gmail.com>
2023-09-11 23:35:46 +0530

    trying to fix dynamic metadata generation

--- app/post/[slug]/layout.tsx
+++ app/post/[slug]/layout.tsx
@@ -1,32 +1,32 @@
 import styles from './page.module.scss'
 import Aside from '@/components/AsidePost'
-// import { getPostBySlug } from '@/services'
+import { getPostBySlug, getPosts } from '@/services'
 
-// import type { Metadata, ResolvingMetadata } from 'next'
+import type { Metadata, ResolvingMetadata } from 'next'
 
-// type Props = {
-//   params: { slug: string }
-//   searchParams: { [key: string]: string | string[] | undefined }
-// }
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   // fetch data
-//   const post = await getPostBySlug(params.slug)
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // fetch data
+  const post = await getPostBySlug(params.slug)
 
-//   // optionally access and extend (rather than replace) parent metadata
-//   const previousImages = (await parent).openGraph?.images || []
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
 
-//   return {
-//     title: post?.title ?? 'HSB',
-//     // description: post?.summary,
-//     openGraph: {
-//       images: ['/some-specific-page-image.jpg', ...previousImages],
-//     },
-//   }
-// }
+  return {
+    title: post?.title ?? 'HSB',
+    description: post?.summary,
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
+  }
+}
 
 export default function Layout({
   children,
