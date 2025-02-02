a6da4c647e5163c1147292ce20e81c2b68cf8a61
Kartik Thakur <kartikth40@gmail.com>
2025-02-02 20:06:31 +0530

    enhancement in featured posts section

--- components/Home/FeaturedPostCard.tsx
+++ components/Home/FeaturedPostCard.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import styles from '../../app/page.module.scss'
+import Image from 'next/image';
 
-type Props = { post: { title: string; summary: string; slug: string } }
+type Props = { post: { title: string; summary: string; slug: string, featuredImage: {url: string} } }
 
 export default function FeaturedPostCard({ post }: Props) {
-  const { featuredPostCard } = styles
+  const { featuredPostCard,featuredPostCardImageContainer } = styles
   return (
     <Link href={`post/${post.slug}`} className={featuredPostCard}>
+      <div className={featuredPostCardImageContainer}>
+        <Image
+          src={post.featuredImage.url}
+          alt={post.title}
+          style={{ objectFit: 'cover' }}
+          fill
+        />
+      </div>
       <h3>{post.title}</h3>
       <p>
         {post.summary.length < 150
