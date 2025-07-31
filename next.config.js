cb74e58ce9bf4f2b07133af7d2e984ddc6518a60
Kartik Thakur <kartikth40@gmail.com>
2025-07-31 08:51:46 +0530

    updated nextConfig to include new updated hygraph image url

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const nextConfig = {}
 
 module.exports = {
   images: {
-    domains: [
-      'media.graphassets.com',
-      'img.icons8.com',
-      'avatars.githubusercontent.com',
-    ],
+    domains: ['media.graphassets.com', 'img.icons8.com', 'avatars.githubusercontent.com', 'ap-south-1.graphassets.com'],
   },
   // turn on this if graphql Too many request error occurs
   experimental: {
